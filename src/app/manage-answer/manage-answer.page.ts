99eb46d2bb0ba6923a0730dd64bef051fcb2e52e
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2019-02-25 14:32:05 -0300

    Functionalities works

--- src/app/manage-answer/manage-answer.page.ts
+++ src/app/manage-answer/manage-answer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { Validators, FormGroup, FormControl} from '@angular/forms';
+import { AnswerService } from '../services/answer.service';
+import { Answer } from '../../../sdk';
 
 @Component({
   selector: 'app-manage-answer',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageAnswerPage implements OnInit {
 
-  constructor() { }
+  _mode : string = "";
+  _question_id: string = "";
+  _answer_id: string = "";
+  answerForm: FormGroup;
+  answer: Answer = new Answer();
+
+  constructor(
+    public navParams: NavParams,
+    public modalCtrl: ModalController,
+    public answerService: AnswerService
+  ) { }
 
   ngOnInit() {
+    this.answerForm = new FormGroup({
+      answer: new FormControl(this.answer.answer, Validators.required)
+    })
+    let data = this.navParams.get('data');
+    this._mode = data.mode;
+    this._question_id = data.questionId;
+    this._answer_id = data.answerId;
+    if(data.answer){
+      debugger
+      this.answer = data.answer;
+      this.answerForm.patchValue({
+        answer: data.answer.answer
+      })
+    }
+  }
+
+  dismiss() {
+    let data = { 'foo': 'bar' };
+    this.modalCtrl.dismiss(data);
+  }
+
+  onSubmit(value){
+    debugger
+    let data = value;
+    data.questionId = this._question_id;
+    if(this.answer.answer){
+      data.id = this.answer.id;
+      data.positiveVotes = this.answer.positiveVotes;
+      data.negativeVotes = this.answer.negativeVotes;
+      this.answerService.updateAnswer(data)
+      .then( res => this.dismiss())
+    }
+    else{
+      this.answerService.createAnswer(value)
+      .then( res => this.dismiss())
+    }
   }
 
 }
--- src/app/manage-question/manage-question.page.ts
+++ src/app/manage-question/manage-question.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormControl} from '@angular/forms';
 import { QuestionService } from '../services/question.service';
 import { ModalController, NavParams } from '@ionic/angular';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-manage-question',
@@ -17,7 +16,6 @@ export class ManageQuestionPage implements OnInit {
 
   constructor(
     public modalCtrl: ModalController,
-    private route: ActivatedRoute,
     public navParams: NavParams,
     public questionService: QuestionService
   ) { }
